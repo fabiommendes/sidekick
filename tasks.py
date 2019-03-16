186f1ca0a976772252b07ac5e17971778c2fdf2f
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-03-16 16:50:20 -0300

    Updated to use PyPoetry

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 import sys
+
 from invoke import run, task
-from python_boilerplate.tasks import *
 
 
 @task
@@ -10,3 +10,13 @@ def configure(ctx):
     """
 
     run("%s -m pip install .[dev] -r requirements.txt" % sys.executable)
+
+
+@task
+def docs(ctx, watch=False):
+    """
+    Instructions for preparing package for development.
+    """
+
+    method = 'autobuild' if watch else 'build'
+    run(f'sphinx-{method} docs/ build/docs/')
