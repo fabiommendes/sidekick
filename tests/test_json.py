dd887218f7ba24fb2be3c432dcb6ec87fad5f1f1
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-03-30 15:16:21 -0300

    Add tests to json

---
+++ tests/test_json.py
@@ -0,0 +1,27 @@
+import io
+
+import sidekick as sk
+
+Point = sk.Record.define("Point", ["x", "y"])
+MPoint = sk.Record.namespace("MPoint", ["x", "y"])
+
+
+class TestJson:
+    def test_serializes_from_record_types(self):
+        record = sk.record(x=1, y=2)
+
+        assert sk.json.dumps(record) == '{"x": 1, "y": 2}'
+        assert sk.json.dumps(sk.namespace(x=1, y=2)) == '{"x": 1, "y": 2}'
+
+        assert sk.json.dumps(Point(x=1, y=2)) == '{"x": 1, "y": 2}'
+        assert sk.json.dumps(MPoint(x=1, y=2)) == '{"x": 1, "y": 2}'
+
+        file = io.StringIO()
+        sk.json.dump(record, file)
+        assert file.getvalue() == '{"x": 1, "y": 2}'
+
+    def test_load_as_record_type(self):
+        assert sk.record(x=1, y=2) == sk.json.loads('{"x": 1, "y": 2}')
+
+        file = io.StringIO('{"x": 1, "y": 2}')
+        assert sk.record(x=1, y=2) == sk.json.load(file)
