2bf5dd67c73be6cad23072c7171eaa38fef3aded
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-03-31 00:10:45 -0300

    Add the IdMap type

--- src/sidekick/types/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import json
-from .anonymous_record import record, namespace
-from .frozen_dict import FrozenKeyDict, FrozenDict
-from .observable_seq import ObservableSeq
-from .record import Record, Namespace
-from .lazy_list import LazyList
-from .inv_map import InvMap
-from .id_map import IdMap
