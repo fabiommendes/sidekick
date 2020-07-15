18a223440e39a34d0d5ae1056653f7d21586d09c
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2020-07-15 12:29:00 -0300

    Initial work with hypothesis testing

--- sidekick/hypothesis/base.py
+++ sidekick/hypothesis/base.py
@@ -1,14 +1,23 @@
+import random
 import re
+from collections import deque
 from keyword import iskeyword
 
 from hypothesis import strategies as st
+from hypothesis.strategies._internal.core import (
+    defines_strategy,
+    defines_strategy_with_reusable_values,
+)
+
+from ..seq import singleton, Iter
 
 IDENTIFIER_RE = re.compile(r"[^\d\W]\w*", re.ASCII)
 PUBLIC_IDENTIFIER_RE = re.compile(r"[^\d\W_]\w*", re.ASCII)
-
+SEQ_TYPES = (tuple, list, set, frozenset, deque)
 AtomT = bool, int, float, complex, str, bytes, type(None), type(Ellipsis)
 
 
+@defines_strategy
 def atoms(which="basic", finite=False):
     """
     Return atomic Python types.
@@ -46,17 +55,19 @@ def atoms(which="basic", finite=False):
 
     if which == "json":
         # See also:
-        # https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MIN_SAFE_INTEGER
+        # https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+        # /Global_Objects/Number/MIN_SAFE_INTEGER
         add(st.integers(min_value=-(2 ** 53 - 1), max_value=2 ** 53 - 1))
         add(st.just(None))
 
     if which == "basic":
         add(st.just(...))
-        add(st.complex_numbers(allow_nan=not finite, allow_infinity=not finite,))
+        add(st.complex_numbers(allow_nan=not finite, allow_infinity=not finite))
 
     return st.one_of(*strategies)
 
 
+@defines_strategy_with_reusable_values
 def identifiers(allow_private=True, exclude=None):
     """
     Valid Python identifiers.
@@ -70,6 +81,7 @@ def identifiers(allow_private=True, exclude=None):
 
 
 # noinspection PyShadowingNames
+@defines_strategy
 def kwargs(values=atoms(), **kwargs):
     """
     Create dictionaries that represent valid keyword arguments.
@@ -115,5 +127,153 @@ def choice(values):
         ... def check_range(fn, x):
         ...     assert -1 <= fn(x) <= 1
     """
-    values = list(values)
-    return st.integers(min_value=0, max_value=len(values) - 1).map(values.__getitem__)
+    values = tuple(values)
+    n_values = len(values)
+    idxs = st.integers(n_values, 2 * n_values - 1)
+    return idxs.map(lambda i: values[i % n_values])
+
+
+def seqs(elements, **kwargs):
+    """
+    Returns Seq's of elements in sidekick sense.
+
+    Seq can be real Python sequences or iterables.
+    """
+    return st.one_of(sequencies(elements, **kwargs), iterators(elements, **kwargs))
+
+
+def seqs_n(elements, size=None, **kwargs):
+    """
+    Returns sequences alongside their sizes.
+
+    Produces elements like ``seqs(elements, size=n, **kwargs)``, but returns
+    tuples of (n, sequence). The sequence may be an iterator and therefore
+    do not support len().
+    """
+
+    if size is not None:
+        kwargs["min_size"] = kwargs["max_size"] = size
+
+    funcs = (tuple, list, deque, iter, Iter, CustomIterable, CustomIterator)
+    data = st.lists(elements, **kwargs)
+
+    return st.builds(lambda fn, xs: (len(xs), fn(xs)), funcs, data)
+
+
+def iterators(elements, **kwargs):
+    """
+    Map iter() function to sequences. Return an iterator that can be immediately
+    used with the next() function.
+    """
+    return sequencies(elements, kind=list, **kwargs).map(iter)
+
+
+def sequencies(elements, kind=SEQ_TYPES, *, size=None, **kwargs):
+    """
+    Create a sized container of uniform type based on lists strategy.
+
+    It simply maps a lists strategy into a possibly different container.
+
+    Args:
+        elements:
+            Strategy used to create elements of container.
+        kind:
+            List of types or single type of acceptable containers.
+        size:
+            Exact size of container.
+
+    Keyword Args:
+        It accepts all arguments of a lists strategy like min_size, max_size,
+        unique and unique_by.
+
+    Examples:
+        >>> st.sized(st.integers())
+        list(integers).map(container)
+    """
+    kind = tuple(singleton(kind))
+    n_kinds = len(kind)
+    if size is not None:
+        kwargs["min_size"] = kwargs["max_size"] = size
+
+    data = st.lists(elements, **kwargs)
+    if n_kinds == 1:
+        return data.map(kind[0])
+    else:
+        return st.builds(lambda xs, fn: fn(xs), data, choice(kind))
+
+
+def funcs(ret, arity=None, *, pure=True, accept_kwargs=True, diversity=50):
+    """
+    Return random functions.
+
+    Args:
+        ret:
+            Strategy for return value.
+        arity:
+            Optional function arity. Specify the number of arguments of created
+            function.
+        pure:
+            If true, function will be "pure" in the sense that it will return the
+            same values for the same arguments.
+        accept_kwargs:
+    """
+
+    def funcs(iterable):
+        outputs = deque((), diversity)
+        results = {}
+
+        def next_result():
+            try:
+                x = next(iterable)
+            except StopIteration:
+                return random.choice(outputs)
+            else:
+                outputs.append(x)
+                return x
+
+        def func(*args, **kwargs):
+            if arity is not None:
+                if len(args) != arity:
+                    raise TypeError("invalid number of arguments")
+            if kwargs and not accept_kwargs:
+                raise TypeError("function does not accept keyword arguments.")
+
+            if pure and not kwargs:
+                key = (*args, *kwargs.items())
+                try:
+                    return results[key]
+                except KeyError:
+                    results[key] = out = next_result()
+                    return out
+
+            return next_result()
+
+        return func
+
+    return st.iterables(ret, min_size=diversity).map(funcs)
+
+
+def preds(arity=None, **kwargs):
+    """
+    Alias to funcs(st.booleans())
+    """
+    return funcs(st.booleans(), arity, **kwargs)
+
+
+class CustomIterable:
+    def __init__(self, data):
+        self.data = data
+
+    def __iter__(self):
+        yield from self.data
+
+
+class CustomIterator:
+    def __init__(self, data):
+        self.data = iter(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.data)
